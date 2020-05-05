89b855431d8c8d7cc0b26fea41c736eda8a890c3
Ivan <ivan1501@gmail.com>
2020-05-05 11:03:24 +0200

    Installed Apollo for state managment.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Post from './components/Post';
+import ApolloClient from 'apollo-boost';
 import './App.css';
 
-    class App extends Component {
-      render() {
-        return (
-          <div>
-            <Header />
-          <div>
-            <Post />
-          </div>
-          </div>
-        );
-      }
-    }
-    
-    export default App;
+const client = new ApolloClient({
+  uri : "http://localhost:4000"
+})
+
+    const App = () => {
+      return (
+          <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+        <section className="App-main">
+          <Post />
+        </section>
+      </div>
+      </ApolloProvider>
+  );
+};
+
+export default App;
