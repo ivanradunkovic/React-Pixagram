d772e637004d09eb7b5186d14db166e4f0adc114
Ivan <ivan1501@gmail.com>
2020-05-06 08:38:27 +0200

    Changes in post component. Queries will render from GraphQL.

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
 import "./Post.css";
 
-    class Post extends Component {
-      render() {
-        return <article className="Post" ref="Post">
-            <header>
-              <div className="Post-user">
-                <div className="Post-user-avatar">
-                  <img src="https://image.shutterstock.com/image-photo/tree-arranged-green-graph-on-600w-182134277.jpg" alt="Ivan" />
-                </div>
-                <div className="Post-user-nickname">
-                  <span>IRa7</span>
-                </div>
-              </div>
-            </header>
-            <div className="Post-image">
-              <div className="Post-image-bg">
-                <img alt="Icon Living" src="https://image.shutterstock.com/image-photo/tree-arranged-green-graph-on-600w-182134277.jpg" />
-              </div>
+class Post extends Component {
+
+  render() {
+    const nickname = this.props.nickname;
+    const avatar = this.props.avatar;
+    const image = this.props.image;
+    const caption = this.props.caption;
+    return (
+      <article className="Post" ref="Post">
+        <header>
+          <div className="Post-user">
+            <div className="Post-user-avatar">
+              <img src={avatar} alt={nickname} />
             </div>
-            <div className="Post-caption">
-              <strong>IRa7</strong> Nature
+            <div className="Post-user-nickname">
+              <span>{nickname}</span>
             </div>
-          </article>;
-        }
-    }
-    export default Post;
+          </div>
+        </header>
+        <div className="Post-image">
+          <div className="Post-image-bg">
+            <img alt={caption} src={image} />
+          </div>
+        </div>
+        <div className="Post-caption">
+          <strong>{nickname}</strong> {caption}
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Post;
