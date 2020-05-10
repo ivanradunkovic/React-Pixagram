6806965772f4a42142a78921d48c00e3a3e98d95
Ivan <ivan1501@gmail.com>
2020-05-10 18:34:10 +0200

    App.js updated.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Post from './components/Post';
-import ApolloClient from 'apollo-boost';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Pusher from 'pusher-js';
+import Header from "./components/Header";
+import Posts from "./components/Posts";
 
 const client = new ApolloClient({
-  uri : "http://localhost:4000"
-})
+  uri: "http://localhost:4000/graphql"
+});
 
-    const App = () => {
-      return (
-          <ApolloProvider client={client}>
+class App extends Component{
+  constructor(){
+    super();
+    //Connect to pusher
+    this.pusher = new Pusher("PUSHER_APP_KEY", {
+      cluster: "eu",
+      encrypted: true
+    });
+  }
+
+  componentDidMount(){
+    if ('actions' in Notification.prototype) {
+      console.log('You can enjoy notification feature');
+    } else {
+      console.log('Sorry notifications are NOT supported on your browser');
+    }
+  }
+  render(){
+    return (
+      <ApolloProvider client={client}>
         <div className="App">
           <Header />
-        <section className="App-main">
-          <Post />
-        </section>
-      </div>
+          <section className="App-main">
+            {/* pass the pusher object to the posts component */}
+            <Posts pusher={this.pusher} apollo_client={client}/>
+          </section>
+        </div>
       </ApolloProvider>
-  );
-};
+    );
+  }
+}
 
 export default App;
